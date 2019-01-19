937393b75dffe074e77a51f04968fccdb93b646b
Pranita Gandhi <pranita_gandhi@hotmail.com>
2019-01-19 15:54:52 +0000

    Test pie chart for average scores per gender in each subject area

--- static/js/graph.js
+++ static/js/graph.js
@@ -6,9 +6,10 @@ function makeGraphs(error, studentData) {
     var ndx = crossfilter(studentData);
 
 
-    show_ethnicity_selector(ndx);   
+    show_ethnicity_selector(ndx);
     show_gender_balance(ndx);
-    
+    show_test_scores_by_gender(ndx);
+
     dc.renderAll();
 
 
@@ -17,7 +18,7 @@ function makeGraphs(error, studentData) {
 function show_ethnicity_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('ethnicity'));
     var group = dim.group();
-    
+
     dc.selectMenu("#ethnicity-selector")
         .dimension(dim)
         .group(group);
@@ -32,7 +33,7 @@ function show_gender_balance(ndx) {
     dc.barChart("#gender-balance")
         .width(400)
         .height(300)
-        .margins({top: 10, right: 50, bottom: 30, left: 50})
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
         .dimension(dim)
         .group(group)
         .transitionDuration(500)
@@ -43,4 +44,15 @@ function show_gender_balance(ndx) {
         .yAxis().ticks(20);
 }
 
+function show_test_scores_by_gender(ndx) {
 
+    var name_dim = ndx.dimension(dc.pluck('gender'));
+    var total_math_score_gender = name_dim.group().reduceSum(dc.pluck('math_score'));
+
+    dc.pieChart('#gender-math-scores')
+        .height(330)
+        .radius(90)
+        .transitionDuration(1500)
+        .dimension(name_dim)
+        .group(total_math_score_gender);
+}
